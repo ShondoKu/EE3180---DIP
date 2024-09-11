318da646ba7a9385c099a58c84348117753479e8
Your Name <you@example.com>
2024-09-12 01:54:31 +0800

    added temp and humid

--- Codes/Test/src/main.cpp
+++ Codes/Test/src/main.cpp
@@ -1,54 +1,91 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 #include <SoftwareSerial.h>
 #define SoundSensorPin A0
 #define VREF 3
-#define PMS7003_TX D5
-#define PMS7003_RX D6
+#define PMS7003_TX 12
+#define PMS7003_RX 14
 #define PMS7003_PREAMBLE_1  0x42 // From PMS7003 datasheet
 #define PMS7003_PREAMBLE_2  0x4D
 #define PMS7003_DATA_LENGTH 31
 
 SoftwareSerial _serial(PMS7003_TX, PMS7003_RX); //RX,TX
-int _pm1,_pm25,pm10;
+
+Adafruit_BME280 bme;
+int _pm1,_pm25,_pm10;
+float _dB;
+float _temperature, _humidity;
 
 
-const char* ssid = "Sean";
-const char* password = "whatpassword";
+const char* ssid = "TP-LINK_0248";
+const char* password = "35353577";
 String TS_apiKey = "EGLOW3A9ZEGHGOHD";
 const char* TS_server = "api.thingspeak.com";
 
 WiFiClient client;
 
+void uploadCloud(float dB, int pm1, int pm25, int pm10, float temp, float humid) {
+  if (client.connect(TS_server, 80))
+    {
+      String postStr = TS_apiKey;
+      postStr += "&field1=";
+      postStr += String(dB);
+      postStr += "&field2=";
+      postStr += String(pm1);
+      postStr += "&field3=";
+      postStr += String(pm25);
+      postStr += "&field4=";
+      postStr += String(pm10);
+      postStr += "&field5=";
+      postStr += String(temp);
+      postStr += "&field6=";
+      postStr += String(humid);
+      postStr += "r\n";
+      
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: api.thingspeak.com\n");
+      client.print("Connection: close\n");
+      client.print("X-THINGSPEAKAPIKEY: " + TS_apiKey + "\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(postStr.length());
+      client.print("\n\n");
+      client.print(postStr);
+      
+      Serial.println("Data Send to Thingspeak");
+    }
+      client.stop();
+      Serial.println("Waiting...");
+      Serial.println();
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid,password);
-  while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.print(".");
-  }
-  //print a new line, then print WiFi connected and the IP address
-  Serial.println("");
-  Serial.println("WiFi connected");
-  // Print the IP address
-  //Serial.println(WiFi.localIP());
+      delay(2000); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  float voltageValue, dB;
-  voltageValue = analogRead(SoundSensorPin) / 1024.0 * VREF;
-  dB = voltageValue * 50.0;
-  Serial.print("dB Value: ");  
-  Serial.print(dB);
-  Serial.println("  dB");   
+void readTempHumdSensor(){
+  _temperature = bme.readTemperature();
+  _humidity = bme.readHumidity();
+  Serial.print("Temperature: ");  
+  Serial.print(_temperature);
+  Serial.println("  °C");   
   Serial.println();
+  Serial.print("Humidity: ");  
+  Serial.print(_humidity);
+  Serial.println("  %");   
+  Serial.println();
+}
 
+void readdBSensor(){
+  float voltageValue;
+    voltageValue = analogRead(SoundSensorPin) / 1024.0 * VREF;
+    _dB = voltageValue * 50.0;
+    Serial.print("dB Value: ");  
+    Serial.print(_dB);
+    Serial.println("  dB");   
+    Serial.println();
+}
+void readPMSSensor() {
   int checksum = 0;
   unsigned char pms[32] = {0,};
 
@@ -81,38 +118,46 @@ void loop() {
     _pm1  = makeWord(pms[10],pms[11]);
     _pm25 = makeWord(pms[12],pms[13]);
     _pm10 = makeWord(pms[14],pms[15]);
-  }		
- 
-
-  if (client.connect(TS_server, 80))
-  {
-    String postStr = TS_apiKey;
-    postStr += "&field1=";
-    postStr += String(dB);
-    postStr += "&field2=";
-    postStr += String(_pm1);
-    postStr += "&field3=";
-    postStr += String(_pm25);
-    postStr += "&field4=";
-    postStr += String(_pm10);
-    postStr += "r\n";
-    
-    client.print("POST /update HTTP/1.1\n");
-    client.print("Host: api.thingspeak.com\n");
-    client.print("Connection: close\n");
-    client.print("X-THINGSPEAKAPIKEY: " + TS_apiKey + "\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\n");
-    client.print("Content-Length: ");
-    client.print(postStr.length());
-    client.print("\n\n");
-    client.print(postStr);
-    
-    Serial.println("Data Send to Thingspeak");
   }
-    client.stop();
-    Serial.println("Waiting...");
- 
-    delay(2000); 
-  
+  Serial.print("PM1 Value: ");  
+  Serial.print(_pm1);
+  Serial.println("  µg/m³");   
+  Serial.println();
+  Serial.print("PM2.5 Value: ");  
+  Serial.print(_pm25);
+  Serial.println("  µg/m³");   
+  Serial.println();
+  Serial.print("PM10 Value: ");  
+  Serial.print(_pm10);
+  Serial.println("  µg/m³");   
+  Serial.println();		
 }
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  _serial.begin(9600);
+  bme.begin(0x76);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+  //print a new line, then print WiFi connected and the IP address
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  //Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readdBSensor();
+  readPMSSensor();
+  readTempHumdSensor();
+  uploadCloud(_dB,_pm1,_pm25,_pm10,_temperature,_humidity);
+}
+
+
 
