618f482965867e57951897b7c488a23da7c7b407
Sean Ku <seanku10@gmail.com>
2024-09-11 16:22:07 +0800

    PMS and dB

--- Codes/Test/src/main.cpp
+++ Codes/Test/src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Wire.h>
-#include "MQ135.h"
+#include <SoftwareSerial.h>
+#define SoundSensorPin A0
+#define VREF 3
+#define PMS7003_TX D5
+#define PMS7003_RX D6
+#define PMS7003_PREAMBLE_1  0x42 // From PMS7003 datasheet
+#define PMS7003_PREAMBLE_2  0x4D
+#define PMS7003_DATA_LENGTH 31
 
-const int MQ135pin = A0;
+SoftwareSerial _serial(PMS7003_TX, PMS7003_RX); //RX,TX
+int _pm1,_pm25,pm10;
 
 
-const char* ssid = "TP-LINK_0248";
-const char* password = "35353577";
+const char* ssid = "Sean";
+const char* password = "whatpassword";
 String TS_apiKey = "EGLOW3A9ZEGHGOHD";
 const char* TS_server = "api.thingspeak.com";
 
@@ -33,21 +41,60 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  MQ135 gasSensor = MQ135(A0);
-  float air_quality = gasSensor.getPPM();
-  
-  //float ppm = 1.0 / (0.03588 * pow(resistance, 1.336));
-  Serial.print("Air Quality: ");  
-  Serial.print(air_quality);
-  Serial.println("  PPM");   
+  float voltageValue, dB;
+  voltageValue = analogRead(SoundSensorPin) / 1024.0 * VREF;
+  dB = voltageValue * 50.0;
+  Serial.print("dB Value: ");  
+  Serial.print(dB);
+  Serial.println("  dB");   
   Serial.println();
+
+  int checksum = 0;
+  unsigned char pms[32] = {0,};
+
+  /**
+   * Search preamble for Packet
+   * Solve trouble caused by delay function
+   */
+  while( _serial.available() && 
+      _serial.read() != PMS7003_PREAMBLE_1 &&
+      _serial.peek() != PMS7003_PREAMBLE_2 ) {
+  }		
+  if( _serial.available() >= PMS7003_DATA_LENGTH ){
+    pms[0] = PMS7003_PREAMBLE_1;
+    checksum += pms[0];
+    for(int j=1; j<32 ; j++){
+      pms[j] = _serial.read();
+      if(j < 30)
+        checksum += pms[j];
+    }
+    _serial.flush();
+    if( pms[30] != (unsigned char)(checksum>>8) 
+      || pms[31]!= (unsigned char)(checksum) ){
+      Serial.println("Checksum error");
+      return;
+    }
+    if( pms[0]!=0x42 || pms[1]!=0x4d ) {
+      Serial.println("Packet error");
+      return;
+    }
+    _pm1  = makeWord(pms[10],pms[11]);
+    _pm25 = makeWord(pms[12],pms[13]);
+    _pm10 = makeWord(pms[14],pms[15]);
+  }		
+ 
 
   if (client.connect(TS_server, 80))
   {
     String postStr = TS_apiKey;
     postStr += "&field1=";
-    postStr += String(air_quality);
+    postStr += String(dB);
+    postStr += "&field2=";
+    postStr += String(_pm1);
+    postStr += "&field3=";
+    postStr += String(_pm25);
+    postStr += "&field4=";
+    postStr += String(_pm10);
     postStr += "r\n";
     
     client.print("POST /update HTTP/1.1\n");
@@ -68,3 +115,4 @@ void loop() {
     delay(2000); 
   
 }
+
